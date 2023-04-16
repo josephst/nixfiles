cac23e2d68f1b5f9f5f0520ce46bb876da2b098c
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2023-04-16 11:53:59 -0400

    trying to fix dnssec error

--- hosts/nixos/nixos-proxmox/services/coredns/default.nix
+++ hosts/nixos/nixos-proxmox/services/coredns/default.nix
@@ -14,6 +14,7 @@ in {
       cache
       loadbalance
       dnssec
+      local
       forward . tls://1.1.1.1 tls://1.0.0.1 {
         tls_servername cloudflare-dns.com
       }
@@ -24,6 +25,12 @@ in {
       # template IN A  {
       #     answer "{{ .Name }} 0 IN A 127.0.0.1"
       # }
+    }
+
+    proxmox.josephstahl.com {
+      template IN A {
+        answer "{{ .Name }} 0 IN A 192.168.1.7"
+      }
     }
     '';
   };
