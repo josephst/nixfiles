fd485df1663af0fb1745dc4019381c52547e494a
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2023-06-26 21:13:25 -0400

    still working on recyclarr pkg

--- pkgs/recyclarr/default.nix
+++ pkgs/recyclarr/default.nix
@@ -1,12 +1,17 @@
-{
-  lib,
-  stdenv,
-  fetchurl,
-  makeWrapper,
-  git,
-  icu,
-  zlib,
-}: let
+{ lib
+, stdenv
+, fetchurl
+, makeWrapper
+, autoPatchelfHook
+, fixDarwinDylibNames
+, recyclarr
+, git
+, icu
+, testers
+, zlib
+,
+}:
+let
   os =
     if stdenv.isDarwin
     then "osx"
@@ -17,63 +22,72 @@
       aarch64-linux = "arm64";
       x86_64-darwin = "x64";
       aarch64-darwin = "arm64";
-    }
-    ."${stdenv.hostPlatform.system}"
-    or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+    }."${stdenv.hostPlatform.system}"
+      or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 
   hash =
     {
       x64-linux_hash = "sha256-EOTxLQrYumnb7khhC2H7Desw9YjpziWOkyBaToE06uw=";
       arm64-linux_hash = "sha256-gcL8WrHkmpqyodluLFplRu7prSk81h+oas50cKOqCOI=";
       x64-osx_hash = "sha256-tQKUsbaVKhP4hMK/byoJt0R7vrXq6fE9bPvZUyWfVVw=";
-      arm64-osx_hash = "sha256-bdpXdLAdHufJzWuv/c+pGNC3HsiXWOA1WIZam25UuQY=";
-    }
-    ."${arch}-${os}_hash";
+      arm64-osx_hash = "sha256-zSHgLXRDB6UA7V0LFgLq9ChqB40IHIJJxRqAYyVFlB8=";
+    }."${arch}-${os}_hash";
 in
-  stdenv.mkDerivation rec {
-    pname = "recyclarr";
-    version = "4.4.1";
+stdenv.mkDerivation rec {
+  pname = "recyclarr";
+  version = "5.1.0";
 
-    src = fetchurl {
-      url = "https://github.com/recyclarr/recyclarr/releases/download/v${version}/recyclarr-${os}-${arch}.tar.xz";
-      inherit hash;
+  src = fetchurl {
+    url = "https://github.com/recyclarr/recyclarr/releases/download/v${version}/recyclarr-${os}-${arch}.tar.xz";
+    inherit hash;
+  };
+
+  sourceRoot = ".";
+
+  nativeBuildInputs = [ makeWrapper ]
+    ++ lib.optional stdenv.hostPlatform.isLinux autoPatchelfHook
+    ++ lib.optional stdenv.hostPlatform.isDarwin fixDarwinDylibNames;
+  buildInputs = [ icu zlib ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp recyclarr $out/bin
+    chmod +x $out/bin/recyclarr
+  '' +
+  lib.optionalString stdenv.isLinux ''
+    patchelf \
+      --add-needed libicui18n.so \
+      --add-needed libicuuc.so \
+      $out/bin/recyclarr
+  '' +
+  ''
+    runHook postInstall
+  '';
+
+  postInstall = ''
+    wrapProgram $out/bin/recyclarr \
+        --prefix PATH : ${lib.makeBinPath [git]} \
+        --set NATIVE_DLL_SEARCH_DIRECTORIES ${lib.makeLibraryPath [icu zlib]} \
+        --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [icu zlib]}
+  '';
+
+  dontStrip = true; # stripping messes up dotnet single-file deployment
+
+  passthru = {
+    updateScript = ./update.sh;
+    tests.version = testers.testVersion {
+      package = recyclarr;
     };
+  };
 
-    setSourceRoot = "sourceRoot=`pwd`"; # recyclarr extracts a single file, no folders
-
-    nativeBuildInputs = [makeWrapper];
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/bin
-      cp recyclarr $out/bin
-      chmod +x $out/bin/recyclarr
-
-      runHook postInstall
-    '';
-
-    postInstall = ''
-      wrapProgram $out/bin/recyclarr \
-          --prefix PATH : ${lib.makeBinPath [git]} \
-          --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [
-        icu
-        zlib
-      ]}
-    '';
-
-    dontStrip = true; # stripping messes up dotnet single-file deployment
-
-    # passthru = {
-    #   updateScript = ./update.sh;
-    # };
-
-    meta = with lib; {
-      description = "Automatically sync TRaSH guides to your Sonarr and Radarr instances";
-      homepage = "https://recyclarr.dev/";
-      changelog = "https://github.com/recyclarr/recyclarr/releases/tag/v${version}";
-      license = licenses.mit;
-      maintainers = with maintainers; [josephst];
-      platforms = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    };
-  }
+  meta = with lib; {
+    description = "Automatically sync TRaSH guides to your Sonarr and Radarr instances";
+    homepage = "https://recyclarr.dev/";
+    changelog = "https://github.com/recyclarr/recyclarr/releases/tag/v${version}";
+    license = licenses.mit;
+    maintainers = with maintainers; [ josephst ];
+    platforms = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+  };
+}
---
+++ pkgs/recyclarr/update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -I nixpkgs=./. -i bash -p curl jq common-updater-scripts gnused nix coreutils
+
+set -euo pipefail
+
+latestVersion="$(curl -s "https://api.github.com/repos/recyclarr/recyclarr/releases?per_page=1" | jq -r ".[0].tag_name" | sed 's/^v//')"
+currentVersion=$(nix-instantiate --eval -E "with import ./. {}; recyclarr.version or (lib.getVersion recyclarr)" | tr -d '"')
+
+if [[ "$currentVersion" == "$latestVersion" ]]; then
+  echo "recyclarr is up-to-date: $currentVersion"
+  exit 0
+fi
+
+update-source-version recyclarr "$latestVersion"
