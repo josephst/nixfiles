fda0ac9000d7f51df7e5f6a4e311fcfee4531508
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2023-08-04 22:14:28 -0400

    wrong rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   joseph = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxKQtKkR7jkse0KMDvVZvwvNwT0gUkQ7At7Mcs9GEop";
   nix-proxmox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINRhc76Xo/X+GXNkWKxdr6kdb/+btJ5tu32i7Tp36tri root@nixos";
   nix-orbstack = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKZKhtJiasDD8qe2IYdjR6gpBlmNb/zt+Q5RobgYSm0o root@nixos-orbstack";
-  allKeys = [joseph nix-proxmox nix-proxmox];
+  allKeys = [joseph nix-proxmox nix-orbstack];
 in {
   "smb.age".publicKeys = allKeys;
   "hashedUserPassword.age".publicKeys = allKeys;
