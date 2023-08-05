ad54d1522fcf1c70b2b72187a8895e7edeb0268c
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2023-08-04 22:05:17 -0400

    agenix: rekey

--- hosts/nixos/common/default.nix
+++ hosts/nixos/common/default.nix
@@ -56,7 +56,7 @@ in {
   ### START personal configuration
 
   # use age encryption key in special location (public & private keys created during nixos install)
-  age.identityPaths = lib.mkDefault [
+  age.identityPaths = [
     "/etc/secrets/agenix/ssh_host_ed25519_key"
     "/etc/ssh/ssh_host_ed25519_key"
   ];
