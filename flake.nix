b79a972e061fa5bfba6fa5833947ceee9eaded81
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2023-10-28 22:14:06 -0400

    try domainname fix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # package repos
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.11";
-    nixpkgs.url = "github:josephst/nixpkgs/fix/netdata-relative-path";
+    nixpkgs.url = "github:josephst/nixpkgs/systemd-domainname";
     # nixpkgs-unstable.follows = "nixpkgs";
 
     # home-manager
