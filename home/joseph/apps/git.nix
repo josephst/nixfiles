e2f58ada18593159fc4d4e17bb49fa8b006849b1
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-04-01 11:11:30 -0400

    let gpg/ssh pick signing key

--- home/joseph/apps/git.nix
+++ home/joseph/apps/git.nix
@@ -7,7 +7,6 @@ in
     enable = true;
     userEmail = "1269177+josephst@users.noreply.github.com";
     userName = "Joseph Stahl";
-    signing.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxKQtKkR7jkse0KMDvVZvwvNwT0gUkQ7At7Mcs9GEop";
     signing.signByDefault = isDarwin; # only sign on macOS for now (simplicity)
     extraConfig = {
       credential.helper = lib.optionalString isDarwin "/usr/local/bin/git-credential-manager";
