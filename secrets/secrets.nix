03360e85afb3db9022ab59414f1b24ac22e861da
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-03-31 20:41:21 -0400

    agenix - rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,16 @@
 let
-  # users
-  joseph = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxKQtKkR7jkse0KMDvVZvwvNwT0gUkQ7At7Mcs9GEop";
+  # users (1 key per user per device)
+  joseph = {
+    main = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxKQtKkR7jkse0KMDvVZvwvNwT0gUkQ7At7Mcs9GEop";
+    joseph-nixos-proxmox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBTyMi+E14e8/droY9+Xg7ORNMMdgH1i6LsfDyKZSy4";
+    joseph-macbook-air = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuLA4wwwupvYW3UJTgOtcOUHwpmRR9gy/N+F6n11d5v";
+  };
 
   # systems
-  nixos-proxmox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdEOEVwz2xyTtGE3EHIy7gTepGCbyqnU5SDd6/2/EkL root@nixos";
   nixos-orbstack = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIpnzK+uR7Bv5OVg04zk3/5TkhjtJYQGQGQOxIr6leeC joseph@nixos-orbstack";
   allKeys = [
-    joseph
-    nixos-proxmox
     nixos-orbstack
-  ];
+  ] ++ builtins.attrValues joseph;
 in
 {
   "dnsApiToken.age".publicKeys = allKeys;
