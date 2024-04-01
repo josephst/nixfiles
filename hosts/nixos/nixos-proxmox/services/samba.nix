bb6a8b448fa0a42d627e4fb4cf3ca71fd928cd1e
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-04-01 13:49:02 -0400

    samba tweaks

--- hosts/nixos/nixos-proxmox/services/samba.nix
+++ hosts/nixos/nixos-proxmox/services/samba.nix
@@ -8,11 +8,6 @@
   # you must add their password using smbpasswd -a <user> as root.
   services.samba = {
     enable = true;
-    package = pkgs.samba.override {
-      # ceph support broken as of 2023-08-13 on nixpkgs-unstable
-      # relies on EOL version of influxdb which is incompatible with latest pandas release
-      enableCephFS = false;
-    };
     securityType = "user";
     extraConfig = ''
       server string = nixos
@@ -42,11 +37,6 @@
       #   "force user" = "joseph";
       #   "force group" = "users";
       # };
-      public = {
-        path = "/mnt/shared";
-        "read only" = "no";
-        "guest ok" = "yes";
-      };
     };
   };
 }
