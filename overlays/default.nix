1ad3488a2694c50bbcc66d2efd4ecc00b6e2fa44
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-03-31 23:10:59 -0400

    disable xz overlay for now

--- overlays/default.nix
+++ overlays/default.nix
@@ -26,10 +26,10 @@
 
   security = final: prev: {
     # for https://github.com/NixOS/nixpkgs/pull/300028, but causes HUGE rebuild
-    xz = prev.xz.overrideAttrs (old: {
-      version = inputs.nixpkgs-staging.legacyPackages.${final.system}.xz.version;
-      src = inputs.nixpkgs-staging.legacyPackages.${final.system}.xz.src;
-    });
+    # xz = prev.xz.overrideAttrs (old: {
+    #   version = inputs.nixpkgs-staging.legacyPackages.${final.system}.xz.version;
+    #   src = inputs.nixpkgs-staging.legacyPackages.${final.system}.xz.src;
+    # });
   };
 
   modifications = final: prev: {
