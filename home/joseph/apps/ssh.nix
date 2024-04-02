fc26a42493116bd5a8beb11fae1a2a11e6224b5f
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-04-02 18:27:23 -0400

    properly map ssh keys

--- home/joseph/apps/ssh.nix
+++ home/joseph/apps/ssh.nix
@@ -2,9 +2,9 @@
 # TODO: try and centralize this section/ refactor into a config.key... module
 # public key is written to disk, private key stays in password manager
 let
-  macbookAirPubKey = pkgs.writeText "macbookAirPubKey" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuLA4wwwupvYW3UJTgOtcOUHwpmRR9gy/N+F6n11d5v joseph@macbookair";
-  nixosProxmoxPubKey = pkgs.writeText "nixosPubKey" "AAAAC3NzaC1lZDI1NTE5AAAAICBTyMi+E14e8/droY9+Xg7ORNMMdgH1i6LsfDyKZSy4 joseph@nixos-proxmox";
-  identityFile = "${macbookAirPubKey} ${nixosProxmoxPubKey}";
+  macbookAirPubKey = pkgs.writeText "macbookAirKey" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuLA4wwwupvYW3UJTgOtcOUHwpmRR9gy/N+F6n11d5v joseph@macbookair";
+  nixosProxmoxPubKey = pkgs.writeText "nixosProxmoxKey" "AAAAC3NzaC1lZDI1NTE5AAAAICBTyMi+E14e8/droY9+Xg7ORNMMdgH1i6LsfDyKZSy4 joseph@nixos-proxmox";
+  identityFile = builtins.map builtins.toString [macbookAirPubKey nixosProxmoxPubKey];
 in
 {
   programs.ssh = {
