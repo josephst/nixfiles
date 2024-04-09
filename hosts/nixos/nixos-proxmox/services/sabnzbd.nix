8d0ced2d5b4d2b43b8385d25784a77d74984c8ac
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-04-09 17:31:08 -0400

    add host to sabnzbd

--- hosts/nixos/nixos-proxmox/services/sabnzbd.nix
+++ hosts/nixos/nixos-proxmox/services/sabnzbd.nix
@@ -6,7 +6,7 @@
 }:
 let
   fqdn = config.networking.fqdn;
-  host_whitelist = "${config.networking.hostName},${fqdn}"; # comma-separated
+  host_whitelist = "${config.networking.hostName},sabnzbd.${fqdn}"; # comma-separated
 in
 {
   services.sabnzbd = {
@@ -25,10 +25,11 @@ in
   };
 
   system.activationScripts.sabnzbd = {
+    # may need to restart sabnzbd (systemctl restart sabnzbd) after this
     text = ''
       if [[ -e ${config.services.sabnzbd.configFile} ]]; then
         # file exists, modify it
-        ${pkgs.gnused}/bin/sed 's/host_whitelist = .*/host_whitelist = ${host_whitelist}/g'
+        ${lib.getBin pkgs.gnused}/bin/sed -i 's/host_whitelist = .*/host_whitelist = ${host_whitelist}/g' ${config.services.sabnzbd.configFile}
       else
         # create new file
         echo "host_whitelist = ${host_whitelist}" > ${config.services.sabnzbd.configFile}
