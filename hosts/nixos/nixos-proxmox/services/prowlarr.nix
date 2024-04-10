2e4aa050824bb2afdd6223a913d411ba48acf88d
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-04-09 21:27:20 -0400

    prowlarr config

--- hosts/nixos/nixos-proxmox/services/prowlarr.nix
+++ hosts/nixos/nixos-proxmox/services/prowlarr.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }:
+{ pkgs, lib, config, ... }:
 let
   fqdn = config.networking.fqdn;
 in
@@ -13,4 +13,14 @@ in
     '';
     useACMEHost = fqdn;
   };
+
+  # system.activationScripts.prowlarr = {
+  #   # may need to restart prowlarr (systemctl restart [prowlarr]) after this
+  #   text = ''
+  #     if [[ -e /var/lib/private/prowlarr/config.xml ]]; then
+  #       # file exists, modify it
+  #       ${lib.getBin pkgs.gnused}/bin/sed -i 's/<AuthenticationMethod>None/<AuthenticationMethod>External/g' /var/lib/private/prowlarr/config.xml
+  #     fi
+  #     '';
+  # };
 }
