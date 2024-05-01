8a219270185cc3087cc4a5212a0486eedacdc3ce
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-05-01 15:40:30 -0400

    expose ollama on network

--- hosts/nixos/nixos-proxmox/services/ollama.nix
+++ hosts/nixos/nixos-proxmox/services/ollama.nix
@@ -7,5 +7,10 @@
 {
   services.ollama = {
     enable = true;
+    listenAddress = "0.0.0.0:11434"; # allow access via tailscale
   };
+
+  networking.firewall.allowedTCPPorts = [
+    11434
+  ];
 }
