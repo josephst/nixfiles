592bfa65ad2a24abbbc55968cb4fab82e9d9f663
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-05-07 18:52:14 -0400

    also forward ssh signing key

--- home/joseph/apps/ssh.nix
+++ home/joseph/apps/ssh.nix
@@ -4,7 +4,9 @@
 let
   macbookAirPubKey = pkgs.writeText "macbookAirKey" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuLA4wwwupvYW3UJTgOtcOUHwpmRR9gy/N+F6n11d5v joseph@macbookair";
   nixosProxmoxPubKey = pkgs.writeText "nixosProxmoxKey" "AAAAC3NzaC1lZDI1NTE5AAAAICBTyMi+E14e8/droY9+Xg7ORNMMdgH1i6LsfDyKZSy4 joseph@nixos-proxmox";
+  sshSigningKey = pkgs.writeText "gitSigningKey" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxKQtKkR7jkse0KMDvVZvwvNwT0gUkQ7At7Mcs9GEop josephst";
   identityFile = builtins.map builtins.toString [
+    sshSigningKey
     macbookAirPubKey
     nixosProxmoxPubKey
   ];
