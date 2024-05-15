72d7b89dbafe1a4cc1b4b94ebdc5728e52d5eb8b
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-05-15 14:51:34 -0400

    astronvim: remove (not working)

--- home/joseph/features/cli/neovim.nix
+++ home/joseph/features/cli/neovim.nix
@@ -5,12 +5,4 @@
     vimAlias = true;
     defaultEditor = true;
   };
-
-  xdg.configFile."nvim".recursive = true;
-  xdg.configFile."nvim".source = pkgs.fetchFromGitHub {
-    owner = "AstroNvim";
-    repo = "AstroNvim";
-    rev = "v4.6.7";
-    hash = "sha256-jvQdFwE2eKe3pmTJtM/Sro0DMirS+QE17w8/CvpXRBo=";
-  };
 }
