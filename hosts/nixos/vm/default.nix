59a97e16016514148e6ac3ccb7ab97e39eb11a2b
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-05-16 08:16:13 -0400

    vm: no mutable users

--- hosts/nixos/vm/default.nix
+++ hosts/nixos/vm/default.nix
@@ -38,6 +38,9 @@
   # Set your time zone.
   time.timeZone = "America/New_York";
 
+  # Use Nix config to manage users
+  users.mutableUsers = false;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
