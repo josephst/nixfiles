8419c45d3096cf47e909adb680fe246bdb2e404f
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-05-16 08:30:50 -0400

    minimal gnome packages

--- hosts/nixos/vm/default.nix
+++ hosts/nixos/vm/default.nix
@@ -60,6 +60,26 @@
     desktopManager.gnome.enable = true;
   };
 
+  # Keep gnome install minimal
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    gnome-terminal
+    gedit # text editor
+    epiphany # web browser
+    geary # email reader
+    evince # document viewer
+    gnome-characters
+    totem # video player
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+  ]);
+
 
   # Configure keymap in X11
   services.xserver.xkb.layout = "us";
