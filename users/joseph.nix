be626ca6de38c65989aa04baf9765dfbe552d465
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-05-19 19:35:26 -0400

    key tweaks

--- users/joseph.nix
+++ users/joseph.nix
@@ -8,7 +8,7 @@ let
   inherit (pkgs.stdenv) isDarwin isLinux;
   # keys = which SSH keys can be used to log in as this user
   keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxKQtKkR7jkse0KMDvVZvwvNwT0gUkQ7At7Mcs9GEop joseph@1password"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBTyMi+E14e8/droY9+Xg7ORNMMdgH1i6LsfDyKZSy4 joseph@nixos-proxmox"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuLA4wwwupvYW3UJTgOtcOUHwpmRR9gy/N+F6n11d5v joseph@macbook-air"
   ];
 in
