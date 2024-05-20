8a35be68e783b82470cea5ea1a7d3aa6bc698ed3
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-05-20 11:36:32 -0400

    more consolidation of keys

---
+++ home/joseph/secrets/default.nix
@@ -0,0 +1,8 @@
+{config, options, ...}: {
+    # new Agenix configuration which is *user-specific* (DISTINCT from the system Agenix config)
+  age = {
+    identityPaths = [
+      "${config.home.homeDirectory}/.ssh/agenix"
+    ] ++ options.age.identityPaths.default;
+  };
+}
--- keys/default.nix
+++ keys/default.nix
@@ -12,9 +12,9 @@
     joseph = {
       # agenix: special key which is used to decrypt user's $HOME across all machines
       agenix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGrRmKWGnpr9wv6Rw5SyHXKPJwMqDS7pR3NHjlepBSxH";
-      macbook-air = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuLA4wwwupvYW3UJTgOtcOUHwpmRR9gy/N+F6n11d5v";
+      Josephs-MacBook-Air = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuLA4wwwupvYW3UJTgOtcOUHwpmRR9gy/N+F6n11d5v";
       # orbstack = "";
-      proxmox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBTyMi+E14e8/droY9+Xg7ORNMMdgH1i6LsfDyKZSy4";
+      nixos-proxmox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBTyMi+E14e8/droY9+Xg7ORNMMdgH1i6LsfDyKZSy4";
     };
   };
 }
