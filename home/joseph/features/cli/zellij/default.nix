3d77322b513fb7eadb3ec6a8494d3d3058198c95
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-05-22 13:03:47 -0500

    zellij: auto-attach

--- home/joseph/features/cli/zellij/default.nix
+++ home/joseph/features/cli/zellij/default.nix
@@ -9,5 +9,9 @@
     enableFishIntegration = false; # see manual settings in fish.nix
   };
 
+  home.sessionVariables = {
+    ZELLIJ_AUTO_ATTACH = true;
+  };
+
   xdg.configFile."zellij/themes/catppuccin.kdl".source = ./catppuccin.kdl;
 }
