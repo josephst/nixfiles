0e3e7951e9eb1cec6177c82dfe183b0d215c1b16
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-05-30 23:20:45 -0400

    ollama: comment about default port

--- hosts/nixos/nixos-proxmox/services/ollama.nix
+++ hosts/nixos/nixos-proxmox/services/ollama.nix
@@ -8,6 +8,7 @@
   services.ollama = {
     enable = true;
     host = "0.0.0.0"; # all address to allow access via tailscale (which also handles auth)
+    # listens on 11434 by default
   };
 
   networking.firewall.allowedTCPPorts = [ 11434 ];
