ec5034862db805b5aef5d89057cb285b62cda11d
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-06-05 18:17:20 -0400

    bump to latest nixpkgs-stable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # package repos
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-staging.url = "github:nixos/nixpkgs/staging-next";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
