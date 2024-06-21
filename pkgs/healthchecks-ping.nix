4c0afb3bbfb3da43779935f1683cf1706a3fc719
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-06-21 17:04:32 -0400

    bash tweaks

--- pkgs/healthchecks-ping.nix
+++ pkgs/healthchecks-ping.nix
@@ -14,7 +14,11 @@ pkgs.writeShellApplication {
     EXIT=''${2:-0}
     NAME=$3
 
+    set +e
     OUTPUT=$(systemctl status "$NAME" -l -n 1000 | tail --bytes 100000)
-    curl -fsS -m 10 -v --retry 5 "https://hc-ping.com/''${UUID}/''${EXIT}" --data-raw "$OUTPUT"
+    set -e
+
+    # curl -fsS -m 10 -v --retry 5 "https://hc-ping.com/''${UUID}/''${EXIT}" --data-raw "$OUTPUT"
+    curl -fsS -m 10 -v --retry 5 "https://hc-ping.com/''${UUID}/''${EXIT}"
   '';
 }
