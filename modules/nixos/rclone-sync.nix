a9ee39e4e4238746393a74222618d5b137616611
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-06-22 14:59:07 -0400

    rclone - systemd log

--- modules/nixos/rclone-sync.nix
+++ modules/nixos/rclone-sync.nix
@@ -149,6 +149,7 @@ in
           ${cfg.package}/bin/rclone \
             --config ''$CREDENTIALS_DIRECTORY/rcloneConf \
             --cache-dir /var/cache/rclone-sync \
+            --log-systemd \
             sync ${cfg.dataDir} $REMOTE ${extraArgs}
         '';
 
