75836bc0e43db85f4469b72fd040fd7091212313
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-07-29 17:44:30 -0400

    start onedrive backups

--- home/joseph/default.nix
+++ home/joseph/default.nix
@@ -41,7 +41,6 @@ in
   myconfig.userSshKeys.identityFileText = userKey; # used in features/cli/ssh.nix
   myconfig.userSshKeys.gitSigningKey = gitSigningKey;
   myconfig.rclone.remotes = [ "onedrive" ]; # sync onedrive using rclone
-  myconfig.rclone.extraArgs = [ "--dry-run" ];
 
   # Home Manager configuration/ options
   home = {
