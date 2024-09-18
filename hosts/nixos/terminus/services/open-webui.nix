849a05e11fa9c3a10599ccd595fbcd4bd619351b
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-09-17 20:40:41 -0400

    disable open-webui

--- hosts/nixos/terminus/services/open-webui.nix
+++ hosts/nixos/terminus/services/open-webui.nix
@@ -4,17 +4,17 @@ let
 in
 {
   services.open-webui = {
-    enable = true;
+    enable = false; # TODO: reenable when no longer broken (0.3.21 is broken)
     openFirewall = true;
     host = "127.0.0.1";
     port = 8082;
   };
 
-  services.caddy.virtualHosts."open-webui.${domain}" = {
-    extraConfig = ''
-      reverse_proxy http://localhost:8082
-      encode gzip
-    '';
-    useACMEHost = domain;
-  };
+  # services.caddy.virtualHosts."open-webui.${domain}" = {
+  #   extraConfig = ''
+  #     reverse_proxy http://localhost:8082
+  #     encode gzip
+  #   '';
+  #   useACMEHost = domain;
+  # };
 }
