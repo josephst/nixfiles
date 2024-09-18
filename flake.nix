055ed4f7abe81938d492a3d9c0879c0eb86ffd18
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-09-17 20:20:39 -0400

    skip checks (breaks deploy-rs)

--- flake.nix
+++ flake.nix
@@ -203,7 +203,7 @@
         };
       };
 
-      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+      # checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
 
       # `nix develop`
       devShells = forAllSystems (
