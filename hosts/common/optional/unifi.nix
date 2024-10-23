f3c571c8a83d5116b63c70e7c59410e949cc52ba
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-10-23 11:03:08 -0400

    use unifi reverse proxy

--- hosts/common/optional/unifi.nix
+++ hosts/common/optional/unifi.nix
@@ -1,9 +1,24 @@
-{pkgs, ...}: {
+{config, pkgs, ...}:
+let
+  inherit (config.networking) domain;
+in {
   services.unifi = {
     enable = true;
     openFirewall = true;
 
     unifiPackage = pkgs.unifi8;
     mongodbPackage = pkgs.mongodb-7_0;
+  };
+
+  services.caddy.virtualHosts."unifi.${domain}" = {
+    extraConfig = ''
+      reverse_proxy https://localhost:8443 {
+        transport http {
+          tls_insecure_skip_verify # we don't verify the controller https cert
+        }
+        header_up - Authorization  # sets header to be passed to the controller
+      }
+    '';
+    useACMEHost = domain;
   };
 }
