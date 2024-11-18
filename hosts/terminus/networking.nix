21fc91cda4a4813551d3b6ca2fa8d3f5f6f07171
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-11-18 11:31:32 -0500

    blocky: also listen on ipv6 network

--- hosts/terminus/networking.nix
+++ hosts/terminus/networking.nix
@@ -19,13 +19,19 @@ _: {
         networkConfig = {
           Address = "192.168.1.10/24";
           Gateway = "192.168.1.1";
-          DNS = " 192.168.1.10";
-
+          DNS = [
+            "192.168.1.10"
+            "::1"
+          ];
+          MulticastDNS = true;
           # accept Router Advertisements for Stateless IPv6 Autoconfiguraton (SLAAC)
           IPv6AcceptRA = true;
         };
         domains = [ "josephstahl.com" ]; # look up ie nixos.josephstahl.com on the local DNS server
         linkConfig.RequiredForOnline = "yes";
+        dhcpV6Config = {
+          UseDNS = false; # don't listen to the ipv6 DNS advertisements from DHCP server, use our own
+        };
       };
     };
   };
