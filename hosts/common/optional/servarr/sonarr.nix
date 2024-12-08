7ce24aa9bcff26efc136ce00ac2de542974ab564
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-12-08 14:24:38 -0500

    sonarr: avoid using EOL version of dotnet

--- hosts/common/optional/servarr/sonarr.nix
+++ hosts/common/optional/servarr/sonarr.nix
@@ -6,8 +6,22 @@ in
   services.sonarr = {
     enable = true;
     group = "media";
-    package = pkgs.sonarr;
+    # package = (pkgs.sonarr.override {
+    #   buildDotnetModule = attrs:
+    #     pkgs.buildDotnetModule (attrs // {
+    #       dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0;
+    #       dotnet-runtime = pkgs.dotnetCorePackages.aspnetcore_8_0;
+    #     });
+    # });
   };
+
+  # TODO: remove when Sonarr updated to use dotnet 8
+  nixpkgs.config.permittedInsecurePackages = [
+    "aspnetcore-runtime-6.0.36"
+    "aspnetcore-runtime-wrapped-6.0.36"
+    "dotnet-sdk-wrapped-6.0.428"
+    "dotnet-sdk-6.0.428"
+  ];
 
   services.caddy.virtualHosts."sonarr.${domain}" = {
     extraConfig = ''
