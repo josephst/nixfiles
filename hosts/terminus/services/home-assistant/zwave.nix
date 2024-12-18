926c913d150c6242e15c9c750f60670eae29da79
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-12-17 19:17:01 -0500

    home-assistant: back to nix module for zwave-js

--- hosts/terminus/services/home-assistant/zwave.nix
+++ hosts/terminus/services/home-assistant/zwave.nix
@@ -1,24 +1,24 @@
 { ... }: {
-  virtualisation.oci-containers.containers = {
-    zwave-js-ui = {
-      image = "zwavejs/zwave-js-ui:latest";
-      ports = [ "8091:8091" "3000:3000" ];
-      pull = "newer";
-      devices = [
-        "/dev/serial/by-id/usb-Zooz_800_Z-Wave_Stick_533D004242-if00:/dev/zwave"
-      ];
-      volumes = [
-        "zwave-js-ui:/usr/src/app/store"
-      ];
-      extraOptions = ["--network=host"];
-    };
-  };
+  # virtualisation.oci-containers.containers = {
+  #   zwave-js-ui = {
+  #     image = "zwavejs/zwave-js-ui:latest";
+  #     ports = [ "8091:8091" "3000:3000" ];
+  #     pull = "newer";
+  #     devices = [
+  #       "/dev/serial/by-id/usb-Zooz_800_Z-Wave_Stick_533D004242-if00:/dev/zwave"
+  #     ];
+  #     volumes = [
+  #       "zwave-js-ui:/usr/src/app/store"
+  #     ];
+  #     extraOptions = ["--network=host"];
+  #   };
+  # };
 
-  networking.firewall.allowedTCPPorts = [ 8091 3000 ];
+  # networking.firewall.allowedTCPPorts = [ 8091 3000 ];
 
-  virtualisation.podman = {
-    enable = true;
-    dockerCompat = true;
-    autoPrune.enable = true;
-  };
+  # virtualisation.podman = {
+  #   enable = true;
+  #   dockerCompat = true;
+  #   autoPrune.enable = true;
+  # };
 }
