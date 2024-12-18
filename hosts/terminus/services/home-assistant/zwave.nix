15882a67f953910a75f6810f5d0fb942c63cc3a4
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-12-17 19:24:47 -0500

    hass: readd zwave config

--- hosts/terminus/services/home-assistant/zwave.nix
+++ hosts/terminus/services/home-assistant/zwave.nix
@@ -1,4 +1,9 @@
-{ ... }: {
+{ config, ... }: {
+  services.zwave-js = {
+    enable = true;
+    serialPort = "/dev/serial/by-id/usb-Zooz_800_Z-Wave_Stick_533D004242-if00";
+    secretsConfigFile = config.age.secrets.zwave-js-keys.path;
+  };
   # virtualisation.oci-containers.containers = {
   #   zwave-js-ui = {
   #     image = "zwavejs/zwave-js-ui:latest";
