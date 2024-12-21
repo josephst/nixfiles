30df80d8673df5d5f60e03c3e77ba5883ba83ce4
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-12-21 03:00:50 -0500

    refactor(healthchecks-ping): remove systemd dependency

--- pkgs/healthchecks-ping.nix
+++ pkgs/healthchecks-ping.nix
@@ -1,9 +1,6 @@
 { writeShellApplication
 , coreutils
 , curl
-, lib
-, systemd
-, stdenv
 , ...
 }:
 
@@ -12,18 +9,13 @@ writeShellApplication {
   runtimeInputs = [
     coreutils
     curl
-  ] ++ lib.optional stdenv.isLinux systemd;
-  # TODO: is there a better way to only build this on linux systems?
+  ];
   text = ''
-    set -x ## DEBUGGING
-
     UUID=$1
     EXIT=''${2:-0}
     NAME=$3
 
-    set +e
     OUTPUT=$(systemctl status "$NAME" -l -n 1000 | tail --bytes 100000)
-    set -e
 
     curl -fsS -m 10 -v --retry 5 "https://hc-ping.com/''${UUID}/''${EXIT}" --data-raw "$OUTPUT"
   '';
