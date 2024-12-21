5ac1c431e8683f6d2d381809a684a5f9d4d82942
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-12-21 05:17:59 -0500

    feat(eza): default options

--- home/joseph/features/base/eza.nix
+++ home/joseph/features/base/eza.nix
@@ -2,6 +2,11 @@ _: {
   programs.eza = {
     enable = true;
     icons = "auto";
+    extraOptions = [
+      "--git"
+      "--group-directories-first"
+      "--header"
+    ];
   };
   home.shellAliases.lt = "eza --tree --level=2 --long --icons --git";
 }
