4215426908428fb766a99d84a8bc2c007ebb9146
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2024-12-24 14:07:46 -0600

    fix(zwave): set dialout group as owner of secrets

--- hosts/nixos/terminus/services/home-assistant/zwave.nix
+++ hosts/nixos/terminus/services/home-assistant/zwave.nix
@@ -7,29 +7,7 @@
 
   age.secrets.zwave-js-keys = {
     file = ../../secrets/zwave-js-keys.json.age;
-    # TODO: use systemd's LoadCredential so that this file doesn't need to be globally readable
-    mode = "774"; # needs to be readable by user zwave-js, but this is created by systemd's DynamicUser
+    mode = "440";
+    group = "dialout"; # zwave service is in dialout supplementary group
   };
-  # virtualisation.oci-containers.containers = {
-  #   zwave-js-ui = {
-  #     image = "zwavejs/zwave-js-ui:latest";
-  #     ports = [ "8091:8091" "3000:3000" ];
-  #     pull = "newer";
-  #     devices = [
-  #       "/dev/serial/by-id/usb-Zooz_800_Z-Wave_Stick_533D004242-if00:/dev/zwave"
-  #     ];
-  #     volumes = [
-  #       "zwave-js-ui:/usr/src/app/store"
-  #     ];
-  #     extraOptions = ["--network=host"];
-  #   };
-  # };
-
-  # networking.firewall.allowedTCPPorts = [ 8091 3000 ];
-
-  # virtualisation.podman = {
-  #   enable = true;
-  #   dockerCompat = true;
-  #   autoPrune.enable = true;
-  # };
 }
