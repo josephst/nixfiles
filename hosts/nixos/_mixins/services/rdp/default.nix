0abba5500337fc749f85c804b16271b4674f2cf6
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2025-03-18 06:23:43 -0400

    fix(terminus): disable sleep

--- hosts/nixos/_mixins/services/rdp/default.nix
+++ hosts/nixos/_mixins/services/rdp/default.nix
@@ -21,4 +21,11 @@ lib.mkIf (lib.elem "${hostname}" installOn) {
   ];
 
   # services.gnome.gnome-remote-desktop.enable = true; # enabled by default if gnome is enabled
+
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 }
