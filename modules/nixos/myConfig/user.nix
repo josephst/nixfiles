b2e713f4949bde18defada1ceee82a1ac1949954
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2025-03-23 02:21:06 -0400

    specify user password

--- modules/nixos/myConfig/user.nix
+++ modules/nixos/myConfig/user.nix
@@ -10,6 +10,10 @@ in
       default = "joseph";
       type = lib.types.str;
     };
+    password = lib.mkOption {
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+    };
     home-manager = {
       enable = lib.mkEnableOption "home-manager" // { default = true; };
       home = lib.mkOption {
@@ -20,6 +24,13 @@ in
   };
 
   config = {
+    assertions = [{
+      assertion = cfg.password != null;
+      message = "You must provide a password file.";
+    }];
+
+    age.secrets.password.file = cfg.password;
+
     home-manager = lib.mkIf cfg.home-manager.enable {
       useGlobalPkgs = lib.mkDefault true;
       useUserPackages = lib.mkDefault true;
@@ -36,6 +47,7 @@ in
       defaultUserShell = pkgs.fish;
       users.${cfg.user} = {
         isNormalUser = true;
+        hashedPasswordFile = config.age.secrets.password.path;
         extraGroups = [ "wheel" "networkmanager" ];
         openssh.authorizedKeys.keys = builtins.attrValues keys.users.joseph;
       };
