83ab140e5b91c02bd4fe32b6ffe5516e3eb5ef7f
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2025-04-01 15:20:18 -0400

    host(terminus): use dhcp

--- hosts/nixos/terminus/networking.nix
+++ hosts/nixos/terminus/networking.nix
@@ -7,19 +7,22 @@ _: {
     networks = {
       "10-lan" = {
         matchConfig.Name = "enp5s0";
-        DHCP = "no";
+        DHCP = "yes";
         networkConfig = {
-          Address = "192.168.1.10/24";
-          Gateway = "192.168.1.1";
+          # Address = "192.168.1.10/24";
+          # Gateway = "192.168.1.1";
           DNS = [
-            "127.0.0.1" # blocky listening here
-            "::1"
+            # "127.0.0.1" # blocky listening here
+            # "::1"
+            "1.1.1.1"
+            "1.0.0.1"
           ];
           MulticastDNS = true;
+          IPv6AcceptRA = true;
         };
         domains = [ "josephstahl.com" ]; # look up ie nixos.josephstahl.com on the local DNS server
         linkConfig = {
-          RequiredForOnline = "yes";
+          RequiredForOnline = "routable";
           Multicast = true;
         };
         ipv6AcceptRAConfig = {
