dc62bb3b4b1c7686d4dcb2f7c7b09970e568ade6
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2025-05-24 15:39:22 -0400

    fix(aider): make configuration less complicated

--- home/joseph/base/aider/default.nix
+++ home/joseph/base/aider/default.nix
@@ -1,22 +1,12 @@
 { pkgs, config, ... }:
-let
-  aiderConf = "${config.xdg.configHome}/aider/aider.conf.yml";
-  aider = pkgs.aider-chat.overrideAttrs (
-    final: prev: {
-      makeWrapperArgs = (prev.makeWrapperArgs or [ ]) ++ [
-        ''--add-flags "--config ${aiderConf}"''
-      ];
-    }
-  );
-in
 {
   # TODO: perhaps make this a shellAbbr instead of overriding the package?
   # slows down builds to re-package aider
-  home.packages = [ aider ];
+  home.packages = [ pkgs.aider-chat ];
 
   age.secrets."aider.conf.yml" = {
     file = ../../secrets/aider.conf.yml.age;
-    path = aiderConf;
+    path = "${config.home.homeDirectory}/.aider.conf.yml";
   };
 
   programs.git.ignores = [ ".aider*" ];
