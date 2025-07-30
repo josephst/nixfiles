838acc47beb6c3221e510e45a3faf91a6d192314
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2025-07-30 09:25:02 -0400

    fix(copyparty): change settings

--- hosts/nixos/terminus/services/copyparty.nix
+++ hosts/nixos/terminus/services/copyparty.nix
@@ -10,6 +10,14 @@ in
 
   services.copyparty = {
     enable = true;
+    settings = {
+      i = "127.0.0.1"; # force reverse proxy
+      e2dsa = true; # enable file indexing and filesystem scanning
+      e2ts = true; # enable multimedia indexing
+      fk = 4; # "fk" enables filekeys (necessary for upget permission) (4 chars long)
+      ansi = true; # color in log messages
+      no-hash = "\.iso$"; # skips hashing file contents if path matches *.iso
+    };
     accounts = {
       joseph.passwordFile = config.age.secrets."copyparty/joseph_password".path;
     };
@@ -19,23 +27,20 @@ in
         path = "/storage/copyparty";
         access = {
           r = "*";
-          rw = [ "joseph" ];
+          A = [ "joseph" ];
         };
-        flags = {
-          # "fk" enables filekeys (necessary for upget permission) (4 chars long)
-          fk = 4;
-          # scan for new files every 60sec
-          scan = 60;
-          # volflag "e2d" enables the uploads database
-          e2d = true;
-          # "d2t" disables multimedia parsers (in case the uploads are malicious)
-          d2t = true;
-          # skips hashing file contents if path matches *.iso
-          nohash = "\.iso$";
+      };
+      "/media" = {
+        path = "/storage/media";
+        access = {
+          r = "*";
+          A = [ "joseph" ];
         };
       };
     };
   };
+
+  users.users.copyparty.extraGroups = [ "media" ];
 
   services.caddy.virtualHosts."copyparty.${domain}" = {
     extraConfig = ''
