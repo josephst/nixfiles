0c40b1579aa8a48ad6be84025568c6fdcaf4b165
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2025-08-02 08:29:41 -0400

    fix: more systemd updates

--- hosts/nixos/terminus/default.nix
+++ hosts/nixos/terminus/default.nix
@@ -58,20 +58,10 @@
 
     # For more detail, see:
     #   https://0pointer.de/blog/projects/watchdog.html
-    watchdog = {
-      # systemd will send a signal to the hardware watchdog at half
-      # the interval defined here, so every 7.5s.
-      # If the hardware watchdog does not get a signal for 15s,
-      # it will forcefully reboot the system.
-      runtimeTime = "15s";
-      # Forcefully reboot if the final stage of the reboot
-      # hangs without progress for more than 30s.
-      # For more info, see:
-      #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
-      rebootTime = "30s";
-      # Forcefully reboot when a host hangs after kexec.
-      # This may be the case when the firmware does not support kexec.
-      kexecTime = "1m";
+    settings.Manager = {
+      KExecWatchdogSec = "1m";
+      RebootWatchdogSec = "30s";
+      RuntimeWatchdogSec = "15s";
     };
 
     sleep.extraConfig = ''
