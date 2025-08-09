14d02f31afef4c3962941a3048cd3c2beaf5d44d
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2025-08-09 10:54:48 -0400

    fix: update zwave-js-server

--- overlays/modifications.nix
+++ overlays/modifications.nix
@@ -7,4 +7,18 @@ final: prev: {
     // {
       smartrent = callPackage ../pkgsLinux/homeassistant-customcomponents/smartrent/package.nix { };
     };
+
+  zwave-js-server = prev.zwave-js-server.overrideAttrs (
+    _: old: rec {
+      version = "3.2.1";
+      src = old.src.override {
+        rev = version;
+        hash = "sha256-oZA+tMYxiWc+PiPiqGEJpEa434CqNjPbutBWjXBgmhw=";
+      };
+      npmDeps = final.fetchNpmDeps {
+        inherit src;
+        hash = "sha256-1JgfXF3kNuUj0jprKBsJSPeFH6ZpqpU4lceTQm5FBgg=";
+      };
+    }
+  );
 }
