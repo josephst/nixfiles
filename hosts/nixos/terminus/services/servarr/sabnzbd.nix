7844eb7b2bf73ae7fb0ff1efdb346250390a047e
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2025-11-28 17:17:25 -0500

    sabnzbd: write configuration defaults

--- hosts/nixos/terminus/services/servarr/sabnzbd.nix
+++ hosts/nixos/terminus/services/servarr/sabnzbd.nix
@@ -1,21 +1,26 @@
 {
-  pkgs,
   config,
-  lib,
   ...
 }:
 let
   inherit (config.networking) domain;
-  host_whitelist = "${config.networking.hostName},sabnzbd.${domain}"; # comma-separated
+  # host_whitelist = "${config.networking.hostName},sabnzbd.${domain}"; # comma-separated
+  starting_config = ''
+    __version__ = 19
+    __encoding__ = utf-8
+    [misc]
+    port = 8082
+    host_whitelist = "${config.networking.hostName},sabnzbd.${domain}"
+  '';
 in
 {
   services.sabnzbd = {
     enable = true;
     group = "media";
-    package = pkgs.sabnzbd;
   };
 
   services.caddy.virtualHosts."sabnzbd.${domain}" = {
+    # remember to edit sabnzbd config to listen on 8082 (otherwise it won't start)
     extraConfig = ''
       reverse_proxy localhost:8082
     '';
@@ -34,18 +39,31 @@ in
     NoNewPrivileges = true;
   };
 
-  system.activationScripts.sabnzbd = {
-    # may need to restart sabnzbd (systemctl restart sabnzbd) after this
-    text = ''
-      if [[ -e ${config.services.sabnzbd.configFile} ]]; then
-        # file exists, modify it
-        ${lib.getBin pkgs.gnused}/bin/sed -i 's/host_whitelist = .*/host_whitelist = ${host_whitelist}/g' ${config.services.sabnzbd.configFile}
-      else
-        # create new file
-        mkdir -p $(dirname ${config.services.sabnzbd.configFile})
-        echo "host_whitelist = ${host_whitelist}" > ${config.services.sabnzbd.configFile}
-        chown ${config.services.sabnzbd.user}:${config.services.sabnzbd.group} ${config.services.sabnzbd.configFile}
-      fi
-    '';
+  systemd.tmpfiles.settings = {
+    "10-sabnzbd" = {
+      "/var/lib/sabnzbd/sabnzbd.ini" = {
+        f = {
+          user = "sabnzbd";
+          group = "media";
+          mode = "0600";
+          argument = starting_config;
+        };
+      };
+    };
   };
+
+  # system.activationScripts.sabnzbd = {
+  #   # may need to restart sabnzbd (systemctl restart sabnzbd) after this
+  #   text = ''
+  #     if [[ -e ${config.services.sabnzbd.configFile} ]]; then
+  #       # file exists, modify it
+  #       ${lib.getBin pkgs.gnused}/bin/sed -i 's/host_whitelist = .*/host_whitelist = ${host_whitelist}/g' ${config.services.sabnzbd.configFile}
+  #     else
+  #       # create new file
+  #       mkdir -p $(dirname ${config.services.sabnzbd.configFile})
+  #       echo "host_whitelist = ${host_whitelist}" > ${config.services.sabnzbd.configFile}
+  #       chown ${config.services.sabnzbd.user}:${config.services.sabnzbd.group} ${config.services.sabnzbd.configFile}
+  #     fi
+  #   '';
+  # };
 }
