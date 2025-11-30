fc6cdd82aa869ddd4d850e433d6f00c08727e1c7
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2025-11-30 13:20:09 -0500

    terminus: again try wol support

--- hosts/nixos/terminus/networking.nix
+++ hosts/nixos/terminus/networking.nix
@@ -2,16 +2,22 @@
 {
   networking = {
     domain = "homelab.josephstahl.com";
-    interfaces = {
-      "enp5s0" = {
-        wakeOnLan.enable = true;
-      };
-    };
   };
 
   systemd.network = {
     enable = true;
     wait-online.anyInterface = true;
+
+    links = {
+      "50-wired" = {
+        matchConfig = {
+          MACAddress = "9c:6b:00:3e:0a:c7";
+        };
+        linkConfig = {
+          WakeOnLan = "magic"; # wake for magic packet
+        };
+      };
+    };
 
     networks = {
       "10-lan" = {
