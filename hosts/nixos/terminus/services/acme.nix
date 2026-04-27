3c5c9f9ca45ad8336a2f82aa473107346ace8ada
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2026-04-27 09:47:01 -0400

    fix(acme): use new credentialsFiles definition
    The name of the environment file is taken from
    https://go-acme.github.io/lego/dns/cloudflare/

--- hosts/nixos/terminus/services/acme.nix
+++ hosts/nixos/terminus/services/acme.nix
@@ -9,14 +9,14 @@ in
 
   security.acme = {
     acceptTerms = true;
-    defaults.email = "grilles_cachets7a@icloud.com";
     defaults.dnsResolver = "1.1.1.1:53"; # can't be local DNS since homelab.josephstahl.com resolves to local IP on LAN
     certs."${domain}" = {
       domain = "*.${domain}";
       extraDomainNames = [ domain ];
       dnsProvider = "cloudflare";
-      # group = config.services.caddy.group;
-      credentialsFile = config.age.secrets.dnsApiToken.path;
+      credentialFiles = {
+        "CF_DNS_API_TOKEN_FILE" = config.age.secrets.dnsApiToken.path;
+      };
       dnsPropagationCheck = true;
     };
   };
