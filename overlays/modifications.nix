e3572197fb9c22a17c4926f0a6b917728de05c40
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2026-04-27 09:47:15 -0400

    chore: bump flake.lock

--- overlays/modifications.nix
+++ overlays/modifications.nix
@@ -8,19 +8,6 @@ final: prev: {
       smartrent = callPackage ../pkgsLinux/homeassistant-customcomponents/smartrent/package.nix { };
     };
 
-  # TODO: remove once https://github.com/NixOS/nixpkgs/pull/510439 is available in nixpkgs-unstable.
-  nushell = prev.nushell.overrideAttrs (old: {
-    checkPhase =
-      builtins.replaceStrings
-        [
-          "--skip=shell::environment::env::path_is_a_list_in_repl"
-        ]
-        [
-          "--skip=shell::environment::env::env_shlvl_in_exec_repl --skip=shell::environment::env::env_shlvl_in_repl --skip=shell::environment::env::path_is_a_list_in_repl"
-        ]
-        old.checkPhase;
-  });
-
   # zwave-js-server = prev.zwave-js-server.overrideAttrs (
   #   _: old: rec {
   #     version = "3.2.1";
