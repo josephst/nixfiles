3cb8e3b33bc681d0b78013efb7b016cc170d5475
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2026-07-11 11:44:05 -0400

    Update Backrest re-evaluation version comment

--- hosts/nixos/anacreon/services/backrest.nix
+++ hosts/nixos/anacreon/services/backrest.nix
@@ -14,7 +14,7 @@ in
       bindAddress = "127.0.0.1";
     };
     caddy.virtualHosts."backrest.${domain}" = {
-      # TODO: multihost with HTTPS doesn't actually work right now, will re-evaluate after 1.13.1 is released (https://github.com/garethgeorge/backrest/pull/1219)
+      # TODO: multihost with HTTPS doesn't actually work right now, will re-evaluate after 1.14.0 is released (https://github.com/garethgeorge/backrest/pull/1219)
       # may need https://github.com/garethgeorge/backrest/commit/d6415931422cb0fb3bd6d14fbe11c37bd97ccf1b to work properly
       extraConfig = ''
         @sync path /v1sync.BackrestSyncService/*
