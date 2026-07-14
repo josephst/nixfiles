4ede8a0d8ed2c954619024624b2a4b1315a18eae
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2026-07-13 21:21:35 -0400

    fix(anacreon): update backrest and acme hostnames

--- hosts/nixos/anacreon/services/backrest.nix
+++ hosts/nixos/anacreon/services/backrest.nix
@@ -4,6 +4,7 @@
 }:
 let
   inherit (config.networking) domain;
+  siteHost = "backrest.${config.networking.hostName}.${domain}";
 in
 {
   services = {
@@ -11,9 +12,7 @@ in
       enable = true;
       bindAddress = "127.0.0.1";
     };
-    caddy.virtualHosts."backrest.${domain}" = {
-      # TODO: Remove the HTTP sync-only fallback once this host runs Backrest >= 1.14.1.
-      # may need https://github.com/garethgeorge/backrest/commit/d6415931422cb0fb3bd6d14fbe11c37bd97ccf1b to work properly
+    caddy.virtualHosts.${siteHost} = {
       extraConfig = ''
         @sync path /v1sync.BackrestSyncService/*
         reverse_proxy @sync h2c://127.0.0.1:9898 {
@@ -26,18 +25,6 @@ in
         reverse_proxy http://127.0.0.1:9898
       '';
       useACMEHost = domain;
-    };
-    caddy.virtualHosts."http://backrest.${domain}" = {
-      extraConfig = ''
-        @sync path /v1sync.BackrestSyncService/*
-        reverse_proxy @sync h2c://127.0.0.1:9898 {
-            flush_interval -1
-            transport http {
-                read_timeout 24h
-                write_timeout 24h
-            }
-        }
-      '';
     };
   };
 }
