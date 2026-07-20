81d772847c7924e3f75efa3c806bce639733f626
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2026-07-20 09:07:45 -0400

    chore: remove obsolete Backrest overlay override

--- overlays/modifications.nix
+++ overlays/modifications.nix
@@ -1,45 +1,4 @@
 final: prev: {
-  # TODO: Remove once nixpkgs-unstable includes Backrest 1.14.1.
-  backrest =
-    let
-      version = "1.14.1";
-      src = prev.fetchFromGitHub {
-        owner = "garethgeorge";
-        repo = "backrest";
-        tag = "v${version}";
-        hash = "sha256-RxjPjvnKy8UM1OXRklJF/HSZ6FMiHWYQBsZ6owMJMF0=";
-        leaveDotGit = true;
-        postFetch = ''
-          cd "$out"
-          git rev-parse HEAD > $out/COMMIT
-          find "$out" -name .git -print0 | xargs -0 rm -rf
-        '';
-      };
-    in
-    prev.backrest.overrideAttrs (oldAttrs: {
-      inherit src version;
-
-      vendorHash = "sha256-yadRulgtcDPthWLeTydcMol/vwriflKvDu7zgoehZCM=";
-
-      passthru = oldAttrs.passthru // {
-        frontend = oldAttrs.passthru.frontend.overrideAttrs (oldFrontendAttrs: {
-          inherit src version;
-
-          buildPhase = ''
-            runHook preBuild
-            export BACKREST_BUILD_VERSION=${version}
-            pnpm build
-            runHook postBuild
-          '';
-
-          pnpmDeps = oldFrontendAttrs.pnpmDeps.overrideAttrs (_: {
-            inherit src version;
-            outputHash = "sha256-y6NYFPepibiTuvPMwyc5cN3TwAc2W7RtPbCmzWDozNQ=";
-          });
-        });
-      };
-    });
-
   # Keep the repository-local SmartRent component compatible with its matching
   # Python library until both versions are available together in nixpkgs.
   home-assistant-custom-components =
