a9e986bef7b8ab192b45608cb2031d016821e9dc
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2026-08-06 10:11:51 -0400

    fix(restic): restrict Caddy access to Tailscale clients (#178)

--- hosts/nixos/terminus/services/caddy.nix
+++ hosts/nixos/terminus/services/caddy.nix
@@ -1,11 +1,6 @@
 {
   services.caddy = {
     enable = true;
-    globalConfig = ''
-      servers {
-        metrics
-      }
-    '';
     openFirewall = true;
     # service-specific config for Caddy reverse-proxying located
     # in each service file (ie sabnzbd.nix, etc.)
