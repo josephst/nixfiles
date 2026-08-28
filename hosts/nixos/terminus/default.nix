1cc03e874d6f9910764a6d6a3ed75b279eb5000a
Joseph Stahl <1269177+josephst@users.noreply.github.com>
2026-08-28 10:54:47 -0400

    feat(terminus): enable graphical printing

--- hosts/nixos/terminus/default.nix
+++ hosts/nixos/terminus/default.nix
@@ -63,6 +63,8 @@ in
         ../common/mixins/xrdp.nix
       ];
 
+      services.printing.enable = true;
+
       environment.sessionVariables.SSH_AUTH_SOCK = "$HOME/.1password/agent.sock";
 
       programs._1password-gui = {
